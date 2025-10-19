363cb664f1ea7ad7eef6b9f4773247877b181ed1
MandatoryTest <saloupio@hotmail.fr>
2025-10-19 18:59:09 +0200

    Initial commit depuis dossier local

--- dvf.py
+++ dvf.py
@@ -25,13 +25,24 @@ def get_mutations_by_parcelle(parcelle_id: str):
         return {"error": str(e)}
 
 def get_communes_du_departement(code_departement="69"):
-    url = f"https://geo.api.gouv.fr/departements/{code_departement}/communes?fields=nom,code"
     try:
-        r = requests.get(url)
-        if r.status_code == 200:
-            return r.json()
-        else:
-            return []
+        # Communes classiques
+        r1 = requests.get(f"https://geo.api.gouv.fr/departements/{code_departement}/communes?fields=nom,code")
+        communes = r1.json() if r1.status_code == 200 else []
+
+        # Arrondissements municipaux
+        r2 = requests.get("https://app.dvf.etalab.gouv.fr/donneesgeo/arrondissements_municipaux-20180711.json")
+        arrondissements = r2.json()["features"] if r2.status_code == 200 else []
+
+        for a in arrondissements:
+            props = a["properties"]
+            if props["code"].startswith(code_departement):
+                communes.append({
+                    "nom": props["nom"],
+                    "code": props["code"]
+                })
+
+        return sorted(communes, key=lambda c: c["nom"])
     except Exception:
         return []
 
